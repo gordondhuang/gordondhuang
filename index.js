ffc370f856f93f7cbe55683c080e3c7d43cf5bd8
GordonDHuang <gordondhuang@gmail.com>
2022-03-09 15:16:58 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var slideIndex = 0;
+carousel();
+
+
+function carousel(){
+    var i;
+    var x = document.getElementsByClassName("slideshow_img");
+    for(i = 0; i < x.length; i++){
+        x[i].style.display = "none"
+    }
+    slideIndex++;
+    if(slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 10000); // change image every 10 seconds
+}
