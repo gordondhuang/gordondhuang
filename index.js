760f2aa5de86b230c4100d42edda1f2b10e967dd
GordonDHuang <gordondhuang@gmail.com>
2022-03-10 21:43:48 -0800

    font change and animation W.I.P.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var slideIndex = 0;
-carousel();
+const nameId = document.getElementById('name');
+const slideContainer = document.getElementsByClassName("slideshow_container");
+var slideshowImg = document.getElementsByClassName("slideshow_img");
+start();
 
+function start(){
+    carousel();
+    slideshowImg.clientHeight = slideshow_container.clientHeight;
+}
 function carousel(){
     var i;
-    var x = document.getElementsByClassName("slideshow_img");
-    for(i = 0; i < x.length; i++){
-        x[i].style.display = "none"
+    for(i = 0; i < slideshowImg.length; i++){
+        slideshowImg[i].style.display = "none"
     }
     slideIndex++;
-    if(slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
+    if(slideIndex > slideshowImg.length) {slideIndex = 1}
+    slideshowImg[slideIndex-1].style.display = "block";
     setTimeout(carousel, 10000); // change image every 10 seconds
+}
+
+function slideShowAni(){
+    
 }
