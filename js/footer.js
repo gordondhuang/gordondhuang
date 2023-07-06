8ed3235cd3516f6a9b5250cd91c493718de04357
GordonDHuang <gordondhuang@gmail.com>
2023-07-06 15:09:27 -0700

    footer script added

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,21 @@
 function footer(){
-    let footerContainer = document.body.createElement('footer')
+    let footerContainer = document.createElement('footer')
 
     footerContainer.innerHTML = `
+
     <div class="icon-section">
         <ul>
-            <li class="iconList"><a href="https://www.instagram.com/gordon.d.huang/"><img class="icons"
-                        src="../images/instagram.png"></a></li>
-            <li class="iconList"><a href="https://www.linkedin.com/in/gordon-huang-802585190/"><img class="icons"
-                        src="../images/linkedin.png"></a></li>
+            <li class="iconList"><a href="https://www.instagram.com/gordon.d.huang/">
+            <img class="icons" src="../images/instagram.png"></a></li>
+            <li class="iconList"><a href="https://www.linkedin.com/in/gordon-huang-802585190/">
+            <img class="icons" src="../images/linkedin.png"></a></li>
         </ul>
     </div>
+
     `
+
+    let body = document.querySelector('body')
+    body.appendChild(footerContainer)
 }
 
 footer();
