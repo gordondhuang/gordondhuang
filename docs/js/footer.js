8e1453bcaafffa6d19490bb48a7efca2af8e824e
Gordon Huang <gordondhuang@gmail.com>
2023-09-17 18:12:15 -0700

    footer completion, main section added, etc.

--- docs/js/footer.js
+++ docs/js/footer.js
@@ -3,21 +3,20 @@ function footer(){
 
     footerContainer.innerHTML = `
 
-    <footer>
-        <div id="footer-sections">
-            <div id="social-media-section">
-                <ul id="social-media-list">
-                <li class="footer-heading">Follow me on social media:</li>
-                <li class="social-media"><a href="https://www.linkedin.com/in/gordondhuang/">LinkedIn</a></li>
-                <li class="social-media"><a href="https://www.instagram.com/gordon.d.huang/">Instagram</a></li>
-                <li class="social-media"><a href="https://github.com/GordonDHuang">GitHub</a></li>
-                </ul>
-            </div>
-            <div class="contact-section">
-                <span class="footer-heading">Need to contact me through email?:</span>
-            </div>
+    <div id="footer-sections">
+        <div id="social-media-section">
+            <ul id="social-media-list">
+            <li class="footer-heading">Follow me on social media:</li>
+            <li class="social-media"><a href="https://www.linkedin.com/in/gordondhuang/">LinkedIn</a></li>
+            <li class="social-media"><a href="https://www.instagram.com/gordon.d.huang/">Instagram</a></li>
+            <li class="social-media"><a href="https://github.com/GordonDHuang">GitHub</a></li>
+            </ul>
         </div>
-    </footer>
+        <div class="contact-section">
+            <span class="footer-heading">Need to follow up through email?:</span>
+            <button id="contact-me-button" onclick="location.href='./contact.html'">Contact me</button>
+        </div>
+    </div>
 
     `
 
