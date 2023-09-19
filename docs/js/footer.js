9592ceab6a208c595dc1ff7247f35aea274ba582
Gordon Huang <gordondhuang@gmail.com>
2023-09-18 22:50:11 -0700

    menu nav icon bar

--- docs/js/footer.js
+++ docs/js/footer.js
@@ -4,18 +4,29 @@ function footer(){
     footerContainer.innerHTML = `
 
     <nav id="footer-sections">
-        <div id="social-media-section">
-            <ul id="social-media-list">
+        <section id="social-media-section">
+            <ul class="footer-list">
             <li class="footer-heading">Follow me on social media:</li>
-            <li class="social-media"><a href="https://www.linkedin.com/in/gordondhuang/">LinkedIn</a></li>
-            <li class="social-media"><a href="https://www.instagram.com/gordon.d.huang/">Instagram</a></li>
-            <li class="social-media"><a href="https://github.com/GordonDHuang">GitHub</a></li>
+            <li class="footer-list-item"><a href="https://www.linkedin.com/in/gordondhuang/">LinkedIn</a></li>
+            <li class="footer-list-item"><a href="https://www.instagram.com/gordon.d.huang/">Instagram</a></li>
+            <li class="footer-list-item"><a href="https://github.com/GordonDHuang">GitHub</a></li>
             </ul>
-        </div>
-        <div class="contact-section">
+        </section>
+
+        <section id="navigation-links-section">
+            <ul class="footer-list"> 
+                <li class="footer-heading">Navigation Links:</li>
+                <li class="footer-list-item"><a href="./index.html">Home</a></li>
+                <li class="footer-list-item"><a href="./about.html">About</a></li>
+                <li class="footer-list-item"><a href="./contact.html">Contact</a></li>
+                <li class="footer-list-item"><a href="./projects.html">Projects</a></li>
+            </ul>
+        </section>
+
+        <section class="contact-section">
             <span class="footer-heading">Need to follow up through email?:</span>
             <button id="contact-me-button" onclick="location.href='./contact.html'">Contact me</button>
-        </div>
+        </section>
     </nav>
     <div>
         <span id="copyright-information"></span>
--- docs/js/header.js
+++ docs/js/header.js
@@ -3,8 +3,14 @@ function header() {
 
     navContainer.innerHTML = `
     
-    <nav>
+    <nav id="navigation">
         <a href="index.html" id="website-name">GH</a>
+        <?xml version="1.0" encoding="UTF-8"?>
+        <button type="button" id="menu-nav-button">
+            <svg class="menu-bar-icon" width="2.5rem" height="2.5rem" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#00000">
+                <path d="M3 5h18M3 12h18M3 19h18" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+        </button>
     </nav>
     `
     
