e17009ef8a7dc3f2838a63d967234acff969ccf3
Gordon Huang <gordondhuang@gmail.com>
2025-08-05 20:12:20 -0700

    modify path for deployment

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/gordondhuang',
+  assetPrefix: '/gordondhuang',
 };
 
 export default nextConfig;
